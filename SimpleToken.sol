69640d3e24d439ead20becdfd19bb58b5d254d3e
ronit-lending-protocol <ronitcrypto41@gmail.com>
2026-05-11 10:37:43 +0530

    Update SimpleToken.sol

--- SimpleToken.sol
+++ SimpleToken.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract SimpleToken {
+    string public name = "Ronit Token";
+    string public symbol = "RTK";
+    uint256 public totalSupply = 1000000 * 10 ** 18;
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function getBalance(address account) public view returns (uint256) {
+        return balanceOf[account];
+    }
+
+    function transfer(address to, uint256 amount) public returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Not enough tokens");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        return true;
+    }
+}
+
 
