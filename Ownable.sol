b52669081082682063f6d1e32d659fba590b4766
ronit-lending-protocol <ronitcrypto41@gmail.com>
2026-05-11 13:01:25 +0530

    Implement Ownable contract with ownership transfer

--- Ownable.sol
+++ Ownable.sol
@@ -1,1 +1,22 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Ownable {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "New owner cannot be zero address");
+        owner = newOwner;
+    }
+}
+
